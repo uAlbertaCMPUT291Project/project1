2b1fff5328ee4360342a2fd362988a453123cb02
LeahGabrielle <olexson@ualberta.ca>
2015-03-17 15:20:58 -0600

    DLR takes input, but does nothing

--- src/DriverLicenceRegistration.java
+++ src/DriverLicenceRegistration.java
@@ -1,4 +1,8 @@
+import java.io.FileInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.sql.*;
 
 //Leah
 public class DriverLicenceRegistration extends ApplicationProgram {
@@ -6,7 +10,34 @@ public class DriverLicenceRegistration extends ApplicationProgram {
 	@Override
 	void run() {
 		Scanner user_input = new Scanner( System.in );
-
+		//make sure licence  number does not exists
+		//make sure sin does exist. If not add person (?)
+		System.out.println("Enter licence number:");
+		String licence = user_input.nextLine();
+		System.out.println("Enter sin:");
+		String sin = user_input.nextLine();
+		System.out.println("Enter class:");
+		String cLass = user_input.nextLine();
+		System.out.println("Enter photo file path:");
+		String filePath = user_input.nextLine();
+		System.out.println("Enter issuing date:");
+		String issueDate = user_input.nextLine();
+		System.out.println("Enter expiring date:");
+		String expireDate = user_input.nextLine();
+		
+		try{
+			PreparedStatement stmt = DatabaseConnection.getConnection().prepareStatement(
+					"insert into drive_licence values ('"+licence+"', '"+sin+"', '"
+					+cLass+"', ?, TO_DATE('"+issueDate+"', 'YYYY-MM-DD', TO_DATE('"
+					+expireDate+", 'YYYY-MM-DD')");
+			File file = new File(filePath);
+			stmt.setBinaryStream(1, new FileInputStream(file), (int)file.length());
+			stmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		};
 	}
 
 }
