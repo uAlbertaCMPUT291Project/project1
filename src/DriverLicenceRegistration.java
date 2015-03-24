203e419b05d3369572d5d437f62852a36ad634c7
LeahGabrielle <olexson@ualberta.ca>
2015-03-24 15:50:21 -0600

    added quit to DLR

--- src/DriverLicenceRegistration.java
+++ src/DriverLicenceRegistration.java
@@ -6,35 +6,55 @@ import java.sql.*;
 
 //Leah
 public class DriverLicenceRegistration extends ApplicationProgram {
-	boolean existance = false;
+	boolean existance = true;
 	String sin = null;
-	int licence = 0;
+	Scanner user_input = new Scanner( System.in );
 	@Override
 	void run() {
-		Scanner user_input = new Scanner( System.in );
-		//make sure licence  number does not exists - generate licence number
-		//make sure sin does exist. If not add person
-		//System.out.println("Enter licence number:");
-		//String licence = user_input.nextLine();
-		
-		while (existance){
-			System.out.println("Enter sin:");
-			sin = user_input.nextLine();
-			existance = checkPersonExistance(sin);
+		System.out.println("Welcome to Driver Licence Registration!\n");
+
+		System.out.println("Please enter licence number. Enter 'exit' to quit.");
+		String licence = user_input.nextLine();
+		if (licence.equalsIgnoreCase("exit")) {
+			System.out.println("You have requested to exit");
+			return;
 		}
-		System.out.println("Enter class: ");
+		
+		System.out.println("Please enter sin. Enter 'exit' to quit.");
+		sin = user_input.nextLine();
+		if (sin.equalsIgnoreCase("exit")) {
+			System.out.println("You have requested to exit");
+			return;
+		}
+		addPerson(sin);
+		
+		System.out.println("Please enter class. Enter 'exit' to quit ");
 		String cLass = user_input.nextLine();
-		System.out.println("Enter photo file path: ");
+		if (cLass.equalsIgnoreCase("exit")) {
+			System.out.println("You have requested to exit");
+			return;
+		}
+		
+		System.out.println("Please enter photo file path. Enter 'exit' to quit.");
 		String filePath = user_input.nextLine();
-		System.out.println("Enter issuing date (YYYY-MM-DD): ");
+		if (filePath.equalsIgnoreCase("exit")) {
+			System.out.println("You have requested to exit");
+			return;
+		}
+		
+		System.out.println("Please enter issuing date (YYYY-MM-DD): ");
 		String issueDate = user_input.nextLine();
-		System.out.println("Enter expiring date (YYYY-MM-DD): ");
+		if (issueDate.equalsIgnoreCase("exit")) {
+			System.out.println("You have requested to exit");
+			return;
+		}
+		
+		System.out.println("Please enter expiring date (YYYY-MM-DD): ");
 		String expireDate = user_input.nextLine();
-		
-		System.out.println("insert into drive_licence values ('"+licence+"', '"+sin+"', '"
-				+cLass+"', ?, TO_DATE('"+issueDate+"', 'YYYY-MM-DD'), TO_DATE('"
-				+expireDate+"', 'YYYY-MM-DD'))");
-		
+		if (expireDate.equalsIgnoreCase("exit")) {
+			System.out.println("You have requested to exit");
+			return;
+		}
 		try{
 			PreparedStatement stmt = DatabaseConnection.getConnection().prepareStatement(
 					"insert into drive_licence values ('"+licence+"', '"+sin+"', '"
@@ -70,6 +90,51 @@ public class DriverLicenceRegistration extends ApplicationProgram {
 			System.out.println(e.getMessage());
 		}
 		return false;
+	}
+	
+	private void addPerson(String sin){
+		//check if person exists
+		existance = checkPersonExistance(sin);
+		//add the person if they do not exist
+		if (existance==false){
+			System.out.println("Person does not exist yet. Press enter to add them or " +
+					"enter 'exit' to quit");
+			String add = user_input.nextLine();
+			if (add.equalsIgnoreCase("exit")) {
+				System.out.println("You have requested to exit");
+				return;
+			} else{
+				System.out.println("Enter owner's name:");
+				String name = user_input.nextLine();
+				System.out.println("Enter owner's height:");
+				String height = user_input.nextLine();
+				System.out.println("Enter owner's weight:");
+				String weight = user_input.nextLine();
+				System.out.println("Enter owner's eye color:");
+				String eyecolor = user_input.nextLine();
+				System.out.println("Enter owner's hair color:");
+				String haircolor = user_input.nextLine();
+				System.out.println("Enter owner's address:");
+				String address = user_input.nextLine();
+				System.out.println("Enter owner's gender (m/f):");
+				String gender = user_input.nextLine();
+				System.out.println("Enter owner's birthday (YYYY-MM-DD):");
+				String birthday = user_input.nextLine();
+
+				try{
+					Statement statement = DatabaseConnection.getConnection().createStatement();
+					String peopleStmt = "insert into people values ('"+sin+"', '"+name+"', "
+							+height+", "+weight+", '"+eyecolor+"', '"+haircolor
+							+"', '"+address+"', '"+gender
+							+"', TO_DATE('"+birthday+"', 'YYYY-MM-DD'))";
+					statement.executeUpdate(peopleStmt);
+					System.out.println("Person added");
+				} catch (SQLException e){
+					//e.printStackTrace();
+					System.out.print(e.getMessage());
+				}
+			}
+		}
 	}
 	
 }
