e2712ef4dbf388cb1341e1163a88a41fa768f796
LeahGabrielle <olexson@ualberta.ca>
2015-03-24 14:58:53 -0600

    moved things into methods

--- src/DriverLicenceRegistration.java
+++ src/DriverLicenceRegistration.java
@@ -6,30 +6,40 @@ import java.sql.*;
 
 //Leah
 public class DriverLicenceRegistration extends ApplicationProgram {
-
+	boolean existance = false;
+	String sin = null;
+	int licence = 0;
 	@Override
 	void run() {
 		Scanner user_input = new Scanner( System.in );
-		//make sure licence  number does not exists
-		//make sure sin does exist. If not add person (?)
-		System.out.println("Enter licence number:");
-		String licence = user_input.nextLine();
-		System.out.println("Enter sin:");
-		String sin = user_input.nextLine();
-		System.out.println("Enter class:");
+		//make sure licence  number does not exists - generate licence number
+		//make sure sin does exist. If not add person
+		//System.out.println("Enter licence number:");
+		//String licence = user_input.nextLine();
+		
+		while (existance){
+			System.out.println("Enter sin:");
+			sin = user_input.nextLine();
+			existance = checkPersonExistance(sin);
+		}
+		System.out.println("Enter class: ");
 		String cLass = user_input.nextLine();
-		System.out.println("Enter photo file path:");
+		System.out.println("Enter photo file path: ");
 		String filePath = user_input.nextLine();
-		System.out.println("Enter issuing date:");
+		System.out.println("Enter issuing date (YYYY-MM-DD): ");
 		String issueDate = user_input.nextLine();
-		System.out.println("Enter expiring date:");
+		System.out.println("Enter expiring date (YYYY-MM-DD): ");
 		String expireDate = user_input.nextLine();
+		
+		System.out.println("insert into drive_licence values ('"+licence+"', '"+sin+"', '"
+				+cLass+"', ?, TO_DATE('"+issueDate+"', 'YYYY-MM-DD'), TO_DATE('"
+				+expireDate+"', 'YYYY-MM-DD'))");
 		
 		try{
 			PreparedStatement stmt = DatabaseConnection.getConnection().prepareStatement(
 					"insert into drive_licence values ('"+licence+"', '"+sin+"', '"
-					+cLass+"', ?, TO_DATE('"+issueDate+"', 'YYYY-MM-DD', TO_DATE('"
-					+expireDate+", 'YYYY-MM-DD')");
+					+cLass+"', ?, TO_DATE('"+issueDate+"', 'YYYY-MM-DD'), TO_DATE('"
+					+expireDate+"', 'YYYY-MM-DD'))");
 			File file = new File(filePath);
 			stmt.setBinaryStream(1, new FileInputStream(file), (int)file.length());
 			stmt.executeUpdate();
@@ -40,4 +50,26 @@ public class DriverLicenceRegistration extends ApplicationProgram {
 		};
 	}
 
+	private boolean checkPersonExistance(String sin) {
+
+		try {
+			Statement stmt = DatabaseConnection.getConnection()
+					.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+							ResultSet.CONCUR_READ_ONLY);
+
+			String query = "select * from people where sin = '"
+					+ sin + "'";
+			ResultSet rs = stmt.executeQuery(query);
+
+			rs.last();
+			if (rs.getRow() > 0) {
+				return true;
+			}
+
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return false;
+	}
+	
 }
--- src/NewVehicleRegistration.java
+++ src/NewVehicleRegistration.java
@@ -10,22 +10,21 @@ import java.util.Scanner;
 //You may assume that all the information about vehicle types has been 
 //loaded in the initial database.
 
+//check data types
 public class NewVehicleRegistration extends ApplicationProgram {
 	boolean existance;
+	String is_primary_owner;
+	Scanner user_input = new Scanner( System.in );
 	@Override
-	void run() {
-		
-		Scanner user_input = new Scanner( System.in );
-		
+	void run() {		
 		//involved tables: vehicle, owner, people
 		//owner sin and vehicle id shared in owner table
-				
-		//while loop
-		//get vehicle id/info from user	
+		
+		//get vehicle info from user	
 		System.out.println("Enter vehicle serial number:");
 		String serial_no = user_input.nextLine();
-		//check if vehicle already exists
 		
+		//check if vehicle already exists
 		existance = checkVehicleExistance(serial_no);
 		
 		if (existance==false){
@@ -37,12 +36,9 @@ public class NewVehicleRegistration extends ApplicationProgram {
 			String year = user_input.nextLine();
 			System.out.println("Enter vehicle color:");
 			String color = user_input.nextLine();
-
 			//idea - display vehicle types and their id's
 			System.out.println("Enter vehicle type id:");
 			String type_id = user_input.nextLine();
-
-
 
 			//add vehicle to vehicle table, if it doesn't exists
 			try{
@@ -54,75 +50,28 @@ public class NewVehicleRegistration extends ApplicationProgram {
 			} catch (SQLException e){
 				//e.printStackTrace();
 				System.out.print(e.getMessage());
-				//try again
 			}
 		} else {
 			System.out.print("vehicle already exists\n");
 			serial_no = null;
-			//ask if quit or try again
+			//ask if quit or try again - loop
 		}
-
-		// get people (check existence - add if neccessary), add owner
 		
-		//person
+		//get person sin
 		System.out.println("Enter owner's sin:");
 		String sin = user_input.nextLine();
 		
-		existance = checkPersonExistance(sin);
-		//check if person exists
-		//check if person is already in people table
-		//if not get and add all their info
-		//person - if necessary (sin does not exist in table)
-		if (existance==false){
-			System.out.println("Enter owner's name:");
-			String name = user_input.nextLine();
-			System.out.println("Enter owner's height:");
-			String height = user_input.nextLine();
-			System.out.println("Enter owner's weight:");
-			String weight = user_input.nextLine();
-			System.out.println("Enter owner's eye color:");
-			String eyecolor = user_input.nextLine();
-			System.out.println("Enter owner's hair color:");
-			String haircolor = user_input.nextLine();
-			System.out.println("Enter owner's address:");
-			String address = user_input.nextLine();
-			System.out.println("Enter owner's gender (m/f):");
-			String gender = user_input.nextLine();
-			System.out.println("Enter owner's birthday (YYYY-MM-DD):");
-			String birthday = user_input.nextLine();
-
-			
-			//add person - not complete
-			//date yyyy-mm-dd
-			try{
-				System.out.println("here");
-				Statement statement = DatabaseConnection.getConnection().createStatement();
-				String peopleStmt = "insert into people values ('"+sin+"', '"+name+"', "
-								+height+", "+weight+", '"+eyecolor+"', '"+haircolor
-								+"', '"+address+"', '"+gender
-								+"', TO_DATE('"+birthday+"', 'YYYY-MM-DD'))";
-				statement.executeUpdate(peopleStmt);
-				System.out.println("Person added");
-			} catch (SQLException e){
-				e.printStackTrace();
-				System.out.print(e.getMessage());
-			}
-		}
+		addPerson(sin);
+		addOwner(serial_no, sin);
 		
-		//is primary owner??
-		System.out.println("Are they the primary owner? (y/n):");
-		String is_primary_owner = user_input.nextLine();
-		
-		//add row to owner
-		try{
-			Statement statement = DatabaseConnection.getConnection().createStatement();
-			String ownerStmt = "insert into owner values ('"+sin+"', '"+serial_no+"', '"
-									+is_primary_owner+"')";
-			statement.executeUpdate(ownerStmt);
-			System.out.println("Vehicle successfully registered");
-		} catch (SQLException e){
-			//e.printStackTrace();
-			System.out.print(e.getMessage());
+		System.out.println("Add another owner? (y/n)");
+		String ans = user_input.nextLine();
+		if (ans == "y"){
+			//get person sin
+			System.out.println("Enter owner's sin:");
+			sin = user_input.nextLine();
+			addPerson(sin);
+			addOwner(serial_no,sin);
 		}
 		
 	}
@@ -170,6 +119,60 @@ public class NewVehicleRegistration extends ApplicationProgram {
 			System.out.println(e.getMessage());
 		}
 		return false;
+	}
+	private void addPerson(String sin){
+		//check if person exists
+		existance = checkPersonExistance(sin);
+		//add the person if they do not exist
+		if (existance==false){
+			System.out.println("Enter owner's name:");
+			String name = user_input.nextLine();
+			System.out.println("Enter owner's height:");
+			String height = user_input.nextLine();
+			System.out.println("Enter owner's weight:");
+			String weight = user_input.nextLine();
+			System.out.println("Enter owner's eye color:");
+			String eyecolor = user_input.nextLine();
+			System.out.println("Enter owner's hair color:");
+			String haircolor = user_input.nextLine();
+			System.out.println("Enter owner's address:");
+			String address = user_input.nextLine();
+			System.out.println("Enter owner's gender (m/f):");
+			String gender = user_input.nextLine();
+			System.out.println("Enter owner's birthday (YYYY-MM-DD):");
+			String birthday = user_input.nextLine();
+
+			try{
+				Statement statement = DatabaseConnection.getConnection().createStatement();
+				String peopleStmt = "insert into people values ('"+sin+"', '"+name+"', "
+								+height+", "+weight+", '"+eyecolor+"', '"+haircolor
+								+"', '"+address+"', '"+gender
+								+"', TO_DATE('"+birthday+"', 'YYYY-MM-DD'))";
+				statement.executeUpdate(peopleStmt);
+				System.out.println("Person added");
+			} catch (SQLException e){
+				e.printStackTrace();
+				System.out.print(e.getMessage());
+			}
+		}
+	}
+	
+	private void addOwner(String serial_no, String sin){
+		//are they primary owner - check if already have a primary owner?
+		System.out.println("Are they the primary owner? (y/n):");
+		is_primary_owner = user_input.nextLine();
+		
+		//add row to owner
+		try{
+			Statement statement = DatabaseConnection.getConnection().createStatement();
+			String ownerStmt = "insert into owner values ('"+sin+"', '"+serial_no+"', '"
+									+is_primary_owner+"')";
+			statement.executeUpdate(ownerStmt);
+			System.out.println("Vehicle successfully registered");
+		} catch (SQLException e){
+			//e.printStackTrace();
+			System.out.print(e.getMessage());
+		}
 	}
 	
 	
