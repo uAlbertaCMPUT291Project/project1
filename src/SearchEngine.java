62aff97c5e52115378e93c5066f84146d3988300
qsjiang <jqiu321@gmail.com>
2015-03-24 00:20:03 -0600

    added sub menu inside search engine

--- src/SearchEngine.java
+++ src/SearchEngine.java
@@ -1,10 +1,71 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 //Jim
 public class SearchEngine extends ApplicationProgram {
 
 	@Override
 	void run() {
-		// TODO Auto-generated method stub
-
+		int userMenuInput = getMenuInput();		
+		if (userMenuInput==1){
+			
+		}
+		
 	}
+	
+	/*private ResultSet getDriveLicenceByLicenceNo(String licenceNo){
+		return runQuery("select ");
+	}*/
+	
+	
+	
+	
+	
+	private int getMenuInput() {
 
+		Scanner scanner = new Scanner(System.in);
+		int userInput = -1;
+
+		while (userInput < 1 || userInput > 6) {
+			System.out.println("Search Engine");
+			System.out.println("Please choose from the following options:");
+			System.out.println("1: Search drive licence by driver licence number");
+			System.out.println("2: Search drive licence by given name");
+			System.out.println("3: Search violation record by driver licence number");
+			System.out.println("4: Search violation record by SIN");
+			System.out.println("5: Search vehicle history by vehicle serial number ");
+			System.out.println("6: Back to main menu");
+
+			if (!scanner.hasNextInt()) {
+				System.out.print("ERROR: ");
+				System.out.println("Not an integer. Please enter an integer");
+				System.out.println();
+				scanner.next();
+				continue;
+			}
+
+			userInput = scanner.nextInt();
+			if (userInput > 6 || userInput < 1) {
+				System.out.print("ERROR: ");
+				System.out.println("Not a valid menu option. Please try again");
+				System.out.println();
+			}
+		}
+
+		return userInput;
+	}
+	
+	
+	/*
+	 * Description: Helper function that runs a given query.
+	 * Input: String query, 
+	 * Output: ResultSet resultSet.
+	 */
+	private ResultSet runQuery(String query) throws SQLException{
+		Connection connection = DatabaseConnection.getConnection();
+		ResultSet resultSet = connection.createStatement().executeQuery(query);
+		return resultSet;		
+	}
 }
