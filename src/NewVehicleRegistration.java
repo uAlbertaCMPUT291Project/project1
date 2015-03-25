6058d9a970c4e11730a058807b6cd4d3c804169a
LeahGabrielle <olexson@ualberta.ca>
2015-03-25 13:58:56 -0600

    finished NVR

--- src/NewVehicleRegistration.java
+++ src/NewVehicleRegistration.java
@@ -14,33 +14,62 @@ import java.util.Scanner;
 //quits
 public class NewVehicleRegistration extends ApplicationProgram {
 	boolean existance;
+	boolean idExist;
 	String is_primary_owner;
 	Scanner user_input = new Scanner( System.in );
 	@Override
 	void run() {		
-		//involved tables: vehicle, owner, people
-		//owner sin and vehicle id shared in owner table
 		
 		//get vehicle info from user	
-		System.out.println("Enter vehicle serial number:");
+		System.out.println("Please enter vehicle serial number. Enter 'exit' to  quit.");
 		String serial_no = user_input.nextLine();
+		if (serial_no.equalsIgnoreCase("exit")) {
+			System.out.println("You have requested to exit");
+			return;
+		}
 		
 		//check if vehicle already exists
 		existance = checkVehicleExistance(serial_no);
 		
 		if (existance==false){
-			System.out.println("Enter vehicle maker:");
+			System.out.println("Please enter vehicle maker. Enter 'exit' to quit.");
 			String maker = user_input.nextLine();
-			System.out.println("Enter vehicle model:");
+			if (maker.equalsIgnoreCase("exit")) {
+				System.out.println("You have requested to exit");
+				return;
+			}			
+			System.out.println("Please enter vehicle model. Enter 'exit' to quit.");
 			String model = user_input.nextLine();
-			System.out.println("Enter vehicle year:");
+			if (model.equalsIgnoreCase("exit")) {
+				System.out.println("You have requested to exit");
+				return;
+			}
+			System.out.println("Please enter vehicle year. Enter 'exit' to quit.");
 			String year = user_input.nextLine();
-			System.out.println("Enter vehicle color:");
+			if (year.equalsIgnoreCase("exit")) {
+				System.out.println("You have requested to exit");
+				return;
+			}
+			System.out.println("Please enter vehicle color. Enter 'exit' to quit.");
 			String color = user_input.nextLine();
-			//idea - display vehicle types and their id's
-			System.out.println("Enter vehicle type id:");
+			if (color.equalsIgnoreCase("exit")) {
+				System.out.println("You have requested to exit");
+				return;
+			}
+			System.out.println("Please enter vehicle type id. Enter 'exit' to quit.");
 			String type_id = user_input.nextLine();
-
+			if (type_id.equalsIgnoreCase("exit")) {
+				System.out.println("You have requested to exit");
+				return;
+			}
+			//check if id exists
+			idExist = checkIdExistance(type_id);
+			if (idExist==false){
+				System.out.println("That is not a valid type_id");
+				System.out.println("Request failed");
+				return;
+			}
+			
 			//add vehicle to vehicle table, if it doesn't exists
 			try{
 				Statement statement = DatabaseConnection.getConnection().createStatement();
@@ -53,21 +82,26 @@ public class NewVehicleRegistration extends ApplicationProgram {
 				System.out.print(e.getMessage());
 			}
 		} else {
-			System.out.print("vehicle already exists\n");
+			System.out.print("Vehicle already exists\n");
 			serial_no = null;
-			//ask if quit or try again - loop
+			System.out.println("Request failed");
+			return;
 		}
 		
 		//get person sin
-		System.out.println("Enter owner's sin:");
+		System.out.println("Please enter owner's sin. Enter 'exit' to quit.");
 		String sin = user_input.nextLine();
+		if (sin.equalsIgnoreCase("exit")) {
+			System.out.println("You have requested to exit");
+			return;
+		}
 		
 		addPerson(sin);
 		addOwner(serial_no, sin);
 		
 		System.out.println("Add another owner? (y/n)");
 		String ans = user_input.nextLine();
-		if (ans == "y"){
+		if (ans.equals("y")){
 			//get person sin
 			System.out.println("Enter owner's sin:");
 			sin = user_input.nextLine();
@@ -124,47 +158,78 @@ public class NewVehicleRegistration extends ApplicationProgram {
 		return false;
 	}
 	
+	private boolean checkIdExistance(String id){
+		try {
+			Statement stmt = DatabaseConnection.getConnection()
+					.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+							ResultSet.CONCUR_READ_ONLY);
+
+			String query = "select * from vehicle where lower(type_id) = lower('"
+					+ id+ "')";
+			ResultSet rs = stmt.executeQuery(query);
+
+			rs.last();
+			if (rs.getRow() > 0) {
+				return true;
+			}
+
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return false;
+	}
+	
 	//adds a person if they do not exist
 	private void addPerson(String sin){
 		//check if person exists
 		existance = checkPersonExistance(sin);
 		//add the person if they do not exist
 		if (existance==false){
-			System.out.println("Enter owner's name:");
-			String name = user_input.nextLine();
-			System.out.println("Enter owner's height:");
-			String height = user_input.nextLine();
-			System.out.println("Enter owner's weight:");
-			String weight = user_input.nextLine();
-			System.out.println("Enter owner's eye color:");
-			String eyecolor = user_input.nextLine();
-			System.out.println("Enter owner's hair color:");
-			String haircolor = user_input.nextLine();
-			System.out.println("Enter owner's address:");
-			String address = user_input.nextLine();
-			System.out.println("Enter owner's gender (m/f):");
-			String gender = user_input.nextLine();
-			System.out.println("Enter owner's birthday (YYYY-MM-DD):");
-			String birthday = user_input.nextLine();
 
-			try{
-				Statement statement = DatabaseConnection.getConnection().createStatement();
-				String peopleStmt = "insert into people values ('"+sin+"', '"+name+"', "
-								+height+", "+weight+", '"+eyecolor+"', '"+haircolor
-								+"', '"+address+"', '"+gender
-								+"', TO_DATE('"+birthday+"', 'YYYY-MM-DD'))";
-				statement.executeUpdate(peopleStmt);
-				System.out.println("Person added");
-			} catch (SQLException e){
-				e.printStackTrace();
-				System.out.print(e.getMessage());
+			System.out.println("Person does not exist yet. Press enter to add them or " +
+					"enter 'exit' to quit");
+			String add = user_input.nextLine();
+			if (add.equalsIgnoreCase("exit")) {
+				System.out.println("You have requested to exit");
+				return;
+			} else{
+
+				System.out.println("Enter owner's name:");
+				String name = user_input.nextLine();
+				System.out.println("Enter owner's height:");
+				String height = user_input.nextLine();
+				System.out.println("Enter owner's weight:");
+				String weight = user_input.nextLine();
+				System.out.println("Enter owner's eye color:");
+				String eyecolor = user_input.nextLine();
+				System.out.println("Enter owner's hair color:");
+				String haircolor = user_input.nextLine();
+				System.out.println("Enter owner's address:");
+				String address = user_input.nextLine();
+				System.out.println("Enter owner's gender (m/f):");
+				String gender = user_input.nextLine();
+				System.out.println("Enter owner's birthday (YYYY-MM-DD):");
+				String birthday = user_input.nextLine();
+
+				try{
+					Statement statement = DatabaseConnection.getConnection().createStatement();
+					String peopleStmt = "insert into people values ('"+sin+"', '"+name+"', "
+							+height+", "+weight+", '"+eyecolor+"', '"+haircolor
+							+"', '"+address+"', '"+gender
+							+"', TO_DATE('"+birthday+"', 'YYYY-MM-DD'))";
+					statement.executeUpdate(peopleStmt);
+					System.out.println("Person added");
+				} catch (SQLException e){
+					e.printStackTrace();
+					System.out.print(e.getMessage());
+				}
 			}
 		}
 	}
 	//adds the ownership to owner table
 	private void addOwner(String serial_no, String sin){
 		//are they primary owner - check if already have a primary owner?
-		System.out.println("Are they the primary owner? (y/n):");
+		System.out.println("Are they the primary owner? (y/n)");
 		is_primary_owner = user_input.nextLine();
 		
 		//add row to owner
